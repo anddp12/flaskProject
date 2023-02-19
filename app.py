14fca2c4bef5cad1f4bd7a1c89beb7b1f19f261e
anddp12 <anddp12@gmail.com>
2023-02-19 23:23:16 +0200

    creation of functions of transitions between pages

--- app.py
+++ app.py
@@ -9,5 +9,25 @@ def home():  # put application's code here
     return render_template("index.html", head1="head")
 
 
+@app.route('/temp')
+def temp():  # put application's code here
+    return render_template("temperature.html")
+
+
+@app.route('/humidity')
+def humidity():  # put application's code here
+    return render_template("humidity.html")
+
+
+@app.route('/meter')
+def meter():  # put application's code here
+    return render_template("meter.html")
+
+
+@app.route('/boiler')
+def boiler():  # put application's code here
+    return render_template("boiler.html")
+
+
 if __name__ == '__main__':
     app.run()
