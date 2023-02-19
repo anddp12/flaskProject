48ef738679a138db2ec95a39e8c286bed43a6ee4
anddp12 <anddp12@gmail.com>
2023-02-19 02:25:36 +0200

    connection bootstrap and creating a site menu

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import url_for, render_template, send_file, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():  # put application's code here
-    return 'Smart home'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
