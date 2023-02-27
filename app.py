439b5737f3393aab42329bbbf0c00729f3e7f1c5
anddp12 <anddp12@gmail.com>
2023-02-27 20:35:10 +0200

    added dynamic page routing

--- app.py
+++ app.py
@@ -10,63 +10,129 @@ app = Flask(__name__)
 @app.route('/')
 def home():  # put application's code here
     title = 'Smart Home'
-    return render_template("index.html", head1="head", title=title)
+    return render_template("index.html", title=title)
 
 
-@app.route('/contact')
-def contact():  # put application's code here
-    title = 'Contact'
-    return render_template("contact.html", head1="head", title=title)
+@app.route('/<text>')
+def text(text):
+    if text == 'about':
+        title = 'About'
+        return render_template('about.html', title=title)
+    elif text == 'contact':
+        title = 'Contact'
+        return render_template('contact.html', title=title)
+    elif text == 'temperature':
+
+        def temp():
+            title = 'Temperature'
+            response = requests.get("http://localhost:5000/api")
+            json_data = json.loads(response.text)
+            temperature = json_data.get('temperature')
+            return render_template("temperature.html", temperature=temperature, title=title)
+
+        return temp()
+    elif text == 'humidity':
+
+        def hum():
+            title = 'Humidity'
+            response = requests.get("http://localhost:5000/api")
+            json_data = json.loads(response.text)
+            humidity = json_data.get('humidity')
+            return render_template("humidity.html", humidity=humidity, title=title)
+
+        return hum()
+    elif text == 'meter':
+        # title = 'Meter'
+
+        def meter():  # put application's code here
+            title = 'Meter'
+            response = requests.get("http://localhost:5000/api")
+            json_data = json.loads(response.text)
+            electricity1 = json_data['meter']['electricity']['reading']
+            electricity2 = json_data['meter']['electricity']['consumption']
+            gas1 = json_data['meter']['gas']['reading']
+            gas2 = json_data['meter']['gas']['consumption']
+            water1 = json_data['meter']['water']['reading']
+            water2 = json_data['meter']['water']['consumption']
+            return render_template("meter.html", electricity1=electricity1, electricity2=electricity2, gas1=gas1,
+                                   gas2=gas2,
+                                   water1=water1, water2=water2, title=title)
+
+        return meter()
+    elif text == 'boiler':
+        def boiler():
+            title = 'Boiler'
+            response = requests.get("http://localhost:5000/api")
+            json_data = json.loads(response.text)
+            boiler1 = json_data['boiler']['isRun']
+            boiler2 = json_data['boiler']['temperature']
+            boiler3 = json_data['boiler']['pressure']
+            return render_template("boiler.html", boiler1=boiler1, boiler2=boiler2, boiler3=boiler3, title=title)
+
+        return boiler()
+    else:
+        return redirect('static/404.html')
 
 
-@app.route('/about')
-def about():  # put application's code here
-    title = 'About'
-    return render_template("about.html", head1="head", title=title)
+# @app.route('/contact')
+# def contact():  # put application's code here
+#     title = 'Contact'
+#     return render_template("contact.html", title=title)
 
 
-@app.route('/temperature')
-def temp():  # put application's code here
-    title = 'Temperature'
-    response = requests.get("http://localhost:5000/api")
-    json_data = json.loads(response.text)
-    temperature = json_data.get('temperature')
-    return render_template("temperature.html", temperature=temperature, title=title)
+# @app.route('/about')
+# def about():  # put application's code here
+#     title = 'About'
+#     return render_template("about.html", title=title)
 
 
-@app.route('/humidity')
-def hum():  # put application's code here
-    title = 'Humidity'
-    response = requests.get("http://localhost:5000/api")
-    json_data = json.loads(response.text)
-    humidity = json_data.get('humidity')
-    return render_template("humidity.html", humidity=humidity, title=title)
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return send_file('static/404.html'), 404
 
 
-@app.route('/meter')
-def meter():  # put application's code here
-    title = 'Meter'
-    response = requests.get("http://localhost:5000/api")
-    json_data = json.loads(response.text)
-    electricity1 = json_data['meter']['electricity']['reading']
-    electricity2 = json_data['meter']['electricity']['consumption']
-    gas1 = json_data['meter']['gas']['reading']
-    gas2 = json_data['meter']['gas']['consumption']
-    water1 = json_data['meter']['water']['reading']
-    water2 = json_data['meter']['water']['consumption']
-    return render_template("meter.html", electricity1=electricity1, electricity2=electricity2, gas1=gas1, gas2=gas2,
-                           water1=water1, water2=water2, title=title)
+# @app.route('/temperature')
+# def temp():  # put application's code here
+#     title = 'Temperature'
+#     response = requests.get("http://localhost:5000/api")
+#     json_data = json.loads(response.text)
+#     temperature = json_data.get('temperature')
+#     return render_template("temperature.html", temperature=temperature, title=title)
 
 
-@app.route('/boiler')
-def boiler():  # put application's code here
-    title = 'Boiler'
-    response = requests.get("http://localhost:5000/api")
-    json_data = json.loads(response.text)
-    boiler1 = json_data['boiler']['isRun']
-    boiler2 = json_data['boiler']['temperature']
-    boiler3 = json_data['boiler']['pressure']
-    return render_template("boiler.html", boiler1=boiler1, boiler2=boiler2, boiler3=boiler3, title=title)
+# @app.route('/humidity')
+# def hum():  # put application's code here
+#     title = 'Humidity'
+#     response = requests.get("http://localhost:5000/api")
+#     json_data = json.loads(response.text)
+#     humidity = json_data.get('humidity')
+#     return render_template("humidity.html", humidity=humidity, title=title)
+
+
+# @app.route('/meter')
+# def meter():  # put application's code here
+#     title = 'Meter'
+#     response = requests.get("http://localhost:5000/api")
+#     json_data = json.loads(response.text)
+#     electricity1 = json_data['meter']['electricity']['reading']
+#     electricity2 = json_data['meter']['electricity']['consumption']
+#     gas1 = json_data['meter']['gas']['reading']
+#     gas2 = json_data['meter']['gas']['consumption']
+#     water1 = json_data['meter']['water']['reading']
+#     water2 = json_data['meter']['water']['consumption']
+#     return render_template("meter.html", electricity1=electricity1, electricity2=electricity2, gas1=gas1, gas2=gas2,
+#                            water1=water1, water2=water2, title=title)
+
+
+# @app.route('/boiler')
+# def boiler():  # put application's code here
+#     title = 'Boiler'
+#     response = requests.get("http://localhost:5000/api")
+#     json_data = json.loads(response.text)
+#     boiler1 = json_data['boiler']['isRun']
+#     boiler2 = json_data['boiler']['temperature']
+#     boiler3 = json_data['boiler']['pressure']
+#     return render_template("boiler.html", boiler1=boiler1, boiler2=boiler2, boiler3=boiler3, title=title)
 
 
 @app.route('/api')
@@ -88,14 +154,6 @@ def api():
     obj['boiler']['pressure'] = round(random.uniform(1.0, 2.0), 1)
 
     return json.dumps(obj)
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return send_file('static/404.html'), 404
-
-
-
 
 
 if __name__ == '__main__':
